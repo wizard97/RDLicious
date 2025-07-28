62776ca49df60dc23fdb1d3b8858fdd919aa4108
Aaron Wisner <awiz@openai.com>
2025-07-28 02:46:23 +0000

    Incremental support.

--- src/ast.rs
+++ src/ast.rs
@@ -2,6 +2,10 @@
 pub enum RootItem {
     Component(ComponentDef),
     Enum(EnumDef),
+    Struct(StructDef),
+    DynPropAssign(DynPropAssign),
+    LocalPropAssign(LocalPropAssign),
+    Udp(UDPDef),
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -20,6 +24,7 @@ pub enum ComponentTypePrimary {
 pub struct ComponentDef {
     pub ctype: ComponentTypePrimary,
     pub name: String,
+    pub params: Vec<ParamDecl>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -35,11 +40,70 @@ pub struct EnumEntry {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ParamDecl {
+    pub data_type: String,
+    pub name: String,
+    pub default: Option<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct StructDef {
+    pub name: String,
+    pub base: Option<String>,
+    pub elems: Vec<StructElem>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct StructElem {
+    pub ty: String,
+    pub name: String,
+    pub is_array: bool,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct DynPropAssign {
+    pub target: Vec<InstanceRefElem>,
+    pub prop: String,
+    pub value: Option<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct LocalPropAssign {
+    pub is_default: bool,
+    pub prop: String,
+    pub value: Option<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct InstanceRefElem {
+    pub ident: String,
+    pub array_suffixes: Vec<Option<Expr>>, // None means unsized '[]'
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Expr {
     Number(String),
     Ident(String),
     Str(String),
+    Bool(bool),
+    EnumLiteral { scope: String, name: String },
+    ArrayLiteral(Vec<Expr>),
+    StructLiteral { name: String, kv: Vec<(String, Expr)> },
     Unary { op: String, rhs: Box<Expr> },
     Binary { op: String, lhs: Box<Expr>, rhs: Box<Expr> },
     Ternary { cond: Box<Expr>, then_br: Box<Expr>, else_br: Box<Expr> },
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct UDPDef {
+    pub name: String,
+    pub attrs: Vec<UDPAttr>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum UDPAttr {
+    Type { data_type: String, is_array: bool },
+    Default(Expr),
+    Usage(Vec<String>),
+    Constraint(String),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,10 @@ fn multiple_components_and_enum() {
         match item {
             ast::RootItem::Component(c) => println!("Component {}", c.name),
             ast::RootItem::Enum(e) => println!("Enum {}", e.name),
+            ast::RootItem::Struct(s) => println!("Struct {}", s.name),
+            ast::RootItem::DynPropAssign(d) => println!("DynProp {}", d.prop),
+            ast::RootItem::LocalPropAssign(p) => println!("LocalProp {}", p.prop),
+            ast::RootItem::Udp(u) => println!("UDP {} ({} attrs)", u.name, u.attrs.len()),
         }
     }
     assert_eq!(root.0.len(), 3);
@@ -164,6 +168,54 @@ fn super_complicated() {
 }
 
 #[test]
+fn dynamic_and_local_props() {
+    let test = r#"
+        reg my_reg {
+            width = 32;
+            depth = 4;
+        };
+        my_reg->reset = 0x10;
+    "#;
+    let root = grammar::RootParser::new().parse(test).unwrap();
+    assert!(root.0.len() >= 2);
+}
+
+#[test]
+fn component_with_params() {
+    let test = "reg MyReg #( number WIDTH=32, number DEPTH ) {}";
+    let comp = grammar::ComponentNamedDefParser::new().parse(test).unwrap();
+    assert_eq!(comp.params.len(), 2);
+    assert_eq!(comp.params[0].name, "WIDTH");
+    assert!(comp.params[0].default.is_some());
+    assert_eq!(comp.params[1].name, "DEPTH");
+}
+
+#[test]
+fn inst_with_param_assign() {
+    let test = r#"
+        reg MyReg #( number W=8 ) {};
+        MyReg #( .W(16) ) inst0;
+    "#;
+    let root = grammar::RootParser::new().parse(test).unwrap();
+    assert_eq!(root.0.len(), 2);
+}
+
+#[test]
+fn udp_def_basic() {
+    let test = r#"
+        property my_prop {
+            type = number;
+            default = 5+3;
+            component = reg|addrmap|all;
+            constraint = componentwidth;
+        };
+    "#;
+    let root = grammar::RootParser::new().parse(test).unwrap();
+    assert_eq!(root.0.len(), 1);
+    match &root.0[0] { ast::RootItem::Udp(u) => { assert_eq!(u.name, "my_prop"); assert_eq!(u.attrs.len(), 4); }, _ => panic!() }
+}
+
+#[test]
 fn expression_basic() {
     use ast::Expr;
     let e = grammar::ExprParser::new().parse("1+2*3").unwrap();
@@ -182,4 +234,30 @@ fn expression_complex() {
         ast::Expr::Ternary { .. } => {}
         _ => panic!("expected ternary"),
     }
+}
+
+#[test]
+fn enum_literal_expr() {
+    let e = grammar::ExprParser::new().parse("my_enum::VALUE").unwrap();
+    match e { ast::Expr::EnumLiteral { scope, name } => { assert_eq!(scope, "my_enum"); assert_eq!(name, "VALUE"); }, _ => panic!() }
+}
+
+#[test]
+fn array_literal_expr() {
+    let e = grammar::ExprParser::new().parse("' {1, 2+3}").unwrap();
+    match e { ast::Expr::ArrayLiteral(v) => assert_eq!(v.len(), 2), _ => panic!() }
+}
+
+#[test]
+fn struct_literal_expr() {
+    let e = grammar::ExprParser::new().parse("foo' { A:1, B:2 }").unwrap();
+    match e { ast::Expr::StructLiteral { name, kv } => { assert_eq!(name, "foo"); assert_eq!(kv.len(), 2); }, _ => panic!() }
+}
+
+#[test]
+fn struct_def_with_elems() {
+    let test = r#"struct foo : bar { number a; mytype b[]; };"#;
+    let root = grammar::RootParser::new().parse(test).unwrap();
+    assert_eq!(root.0.len(), 1);
+    match &root.0[0] { ast::RootItem::Struct(s) => { assert_eq!(s.name, "foo"); assert_eq!(s.base.as_deref(), Some("bar")); assert_eq!(s.elems.len(), 2); assert!(s.elems[1].is_array); }, _ => panic!() }
 }
